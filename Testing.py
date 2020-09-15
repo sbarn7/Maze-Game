81efe34fbb7973b70418c94790f8f0fb099a4f1d
SBarnes <barnessj7@gmail.com>
2020-09-15 14:18:13 -0400

    TestingNewAdd

--- Testing.py
+++ Testing.py
@@ -2,6 +2,7 @@
 #at one point, i'd love to have it randomly generate a starting room
 
 #should try and understand while loops more, that'll probably help
+#testing something new here to see if Git uploads it yay
 
 def Labyrinth():
     print("Hello. You are trapped in a maze. Can you find the way out?")
